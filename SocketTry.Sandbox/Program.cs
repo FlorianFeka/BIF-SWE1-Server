287a10a5918e18e96ab436f9a1f81ea590599264
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2021-01-18 19:47:51 +0100

    refactor

--- SocketTry.Sandbox/Program.cs
+++ SocketTry.Sandbox/Program.cs
@@ -2,13 +2,12 @@
 using System;
 using System.Net;
 
-namespace SocketTry
+namespace SocketTry.Sandbox
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8081);
             using (var server = new HttpServer())
             {
