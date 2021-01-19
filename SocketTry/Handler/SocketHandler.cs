9a4ebb7cd398f5f438a8d5502f6df8f58e088488
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2021-01-19 14:07:45 +0100

    update socket send

--- SocketTry/Handler/SocketHandler.cs
+++ SocketTry/Handler/SocketHandler.cs
@@ -1,6 +1,4 @@
-﻿using SocketTry.Implementations;
-using SocketTry.Utils;
-using System;
+﻿using System;
 using System.Net.Sockets;
 using System.Text;
 
@@ -15,11 +13,10 @@ namespace SocketTry.Handler
         private byte[] _receiveBuffer;
         private int _receiveBufferSize;
 
-        private int _sendRound = 0;
         private byte[] _sendData;
+        private int _sendDataSizeLeft;
+        private int _sentDataSize;
         private int _sendBufferSize;
-
-        private readonly byte _endOfString = (byte)'\0';
 
         internal SocketHandler(Socket socket, int receiveBufferSize, int sendBufferSize)
         {
@@ -86,49 +83,18 @@ namespace SocketTry.Handler
         internal void Send(byte[] data)
         {
             _sendData = data;
-            SendBuffer();
+            _sendDataSizeLeft = data.Length;
+            BeginSend();
         }
 
-        private void SendBuffer()
+        private void BeginSend()
         {
-            try
+            var len = _sendDataSizeLeft;
+            if(len > _sendBufferSize)
             {
-                var lastPart = CreateSendBuffer(out var buffer);
-                _socket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(HandleSend), lastPart);
+                len = _sendBufferSize;
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-                Dispose();
-            }
-        }
-
-        /// <summary>
-        /// Creates a byte array which is part of the data to be sent
-        /// and fills the empty space if there is any with \0 (end of string)
-        /// </summary>
-        /// <param name="buffer"></param>
-        /// <returns>returns <c>true</c> if the created buffer is the last one to be created, otherwise it returns <c>false</c></returns>
-        /// <exception cref="Exception">throws exception when the configured send_buffer_size is bellow 1</exception>
-        private bool CreateSendBuffer(out byte[] buffer)
-        {
-            if (_sendBufferSize < 1) throw new Exception("Too small send_buffer!");
-
-            int from = _sendBufferSize * _sendRound;
-            from = from != 0 ? from++ : 0;
-            if (from >= _sendData.Length) throw new Exception("Data should already be sent!");
-
-            var len = _sendBufferSize;
-            len = len < _sendData.Length ? len : _sendData.Length - from;
-
-            buffer = new byte[_sendBufferSize];
-
-            Array.Copy(_sendData, from, buffer, 0, len);
-
-            if (len < _sendBufferSize) Util.FillEmptySpaceWith<byte>(buffer, _endOfString, len);
-
-            _sendRound++;
-            return (from + len) == _sendData.Length;
+            _socket.BeginSend(_sendData, _sentDataSize, len, SocketFlags.None, new AsyncCallback(HandleSend), null);
         }
 
         private void HandleSend(IAsyncResult result)
@@ -136,9 +102,10 @@ namespace SocketTry.Handler
             if (!_listening) return;
             try
             {
-                _socket.EndSend(result);
-                var lastPart = result.AsyncState as bool?;
-                if (lastPart.HasValue && lastPart.Value)
+                int sentBytes = _socket.EndSend(result);
+                _sendDataSizeLeft -= sentBytes;
+                _sentDataSize += sentBytes;
+                if (_sendDataSizeLeft == 0)
                 {
                     ClearForNewSend();
                     return;
@@ -149,13 +116,15 @@ namespace SocketTry.Handler
                 Console.WriteLine(e.ToString());
                 Dispose();
             }
-            SendBuffer();
+            BeginSend();
         }
 
         private void ClearForNewSend()
         {
             _sendData = null;
             _sendRound = 0;
+            _sendDataSizeLeft = 0;
+            _sentDataSize = 0;
         }
 
         public void Dispose()
